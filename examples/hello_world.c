2bb6a5c6d6ca83fa53660759e6bdfd5cae6a1a1d
James Antill <james@code.and.org>
2007-09-24 00:58:49 -0400

    Make hello world example better

--- examples/hello_world.c
+++ examples/hello_world.c
@@ -8,28 +8,42 @@ static void die(const char *prog_name, const char *msg)
   exit (EXIT_FAILURE);
 }
 
-int main(void)
+/* simplest, just "create" a Ustr from a constant string */
+static void hello_world_one(void)
 {
+  Ustr *hello_world = USTR1(\xC, "Hello world!");
+
+  if (!ustr_io_putfileline(&hello_world, stdout))
+    die("hello_world", strerror(errno));
+}
+
+static void hello_world_two(void)
+{ /* next, create a Ustr from a printf() like format. This needs to be
+   * free'd. */
+  Ustr *out = ustr_dup_fmt("%s %s", "Hello", "world!");
+
+  if (!out || !ustr_io_putfileline(&out, stdout))
+    die("hello_world", strerror(errno));
+
+  ustr_free(out);
+}
+
+static void hello_world_three(void)
+{ /* manually create a Ustr, from multiple parts. Often "significantly faster"
+   * than using ustr_*_fmt(), due to printf overhead. Still needs to allocate
+   * memory, and maybe resize it. Still need to free it. */
   Ustr *hello = USTR1(\5, "Hello");
   Ustr *sp    = USTR1(\1, " ");
   Ustr *world = USTR1(\6, "world!");
-  Ustr *out = NULL;
+  Ustr *out   = ustr_dup_empty();
 
-  out = hello;
-  if (!ustr_io_putfile(&out, stdout))
-    die("hello_world", strerror(errno));
-  out = sp;
-  if (!ustr_io_putfile(&out, stdout))
-    die("hello_world", strerror(errno));
-  out = world;
-  if (!ustr_io_putfileline(&out, stdout))
-    die("hello_world", strerror(errno));
-
-  if (!(out = ustr_dup_empty()))
+  if (!out)
     die("hello_world", strerror(ENOMEM));
+  
   ustr_add(&out, hello);
   ustr_add(&out, sp);
   ustr_add(&out, world);
+  
   if (ustr_enomem(out)) /* check all 3 additions at once */
     die("hello_world", strerror(ENOMEM));
   
@@ -37,6 +51,35 @@ int main(void)
     die("hello_world", strerror(errno));
   
   ustr_free(out);
+}
+
+static void hello_world_four(void)
+{ /* manually create a Ustr, but use "auto" allocated storage
+   * (stack instead of heap). As long as you don't use more than ustr_size()
+   * you don't need to free. Also note that ustr_dup() will now always copy. */
+  Ustr *sp    = USTR1(\1, " ");
+  Ustr *world = USTR1(\6, "world!");
+  char buf_out[1024] = USTR_BEG_FIXED2 "Hello";
+  Ustr *out = USTR_SC_INIT_AUTO(buf_out, USTR_TRUE, 5);
+
+  ustr_add(&out, sp);
+  ustr_add(&out, world);
+
+  /* in this can we know !ustr_enomem() as there is more than enough space */
+  
+  if (!ustr_io_putfileline(&out, stdout))
+    die("hello_world", strerror(errno));
+  
+  /* ustr_free() not needed, because nothing was allocated.
+   * Although it's often good to call it anyway, as it does no harm. */
+}
+
+int main(void)
+{
+  hello_world_one();
+  hello_world_two();
+  hello_world_three();
+  hello_world_four();
   
   return (EXIT_SUCCESS);
 }
