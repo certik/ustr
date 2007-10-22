3b1d9e7c3df27b118b3873ea9312f0fa63dfa2cc
James Antill <james@and.org>
2007-10-22 19:26:48 -0400

    Add more installed tests

--- T-installed/tst_srch.c
+++ T-installed/tst_srch.c
@@ -6,6 +6,12 @@ int main(void)
 
   if (ustr_srch_fwd(s1, 0, USTR1(\2, "23")) != 2)
     return 1;
+
+  /* didn't include a Versioned symbol in 1.0.1 */
+  if (ustr_srch_rep_chr_fwd(s1, 0, '3', 1) != 3)
+    return 1;
+  if (ustr_srch_rep_chr_fwd(s1, 0, '3', 2) != 0)
+    return 1;
   
   return 0;
 }
